0d742e5c3af2fbae9bc9a3875afed79f0a2ecb47
FYLSunghwan <sunghwan519@hotmail.com>
2020-09-12 20:30:47 +0900

    build: buid project structure

--- ios/PianoSampler.swift
+++ ios/PianoSampler.swift
@@ -1,8 +1,22 @@
 @objc(PianoSampler)
 class PianoSampler: NSObject {
 
-    @objc(multiply:withB:withResolver:withRejecter:)
-    func multiply(a: Float, b: Float, resolve:RCTPromiseResolveBlock,reject:RCTPromiseRejectBlock) -> Void {
-        resolve(a*b)
+    @objc(playNote:velocity:)
+    func playNote(_ midiNum: NSInteger, velocity: NSInteger) {
+        print("Swift>playNote")
+    }
+  
+    @objc(stopNote:)
+    func stopNote(_ midiNum: NSInteger) {
+        print("Swift>stopNote")
+    }
+    
+    @objc
+    func prepare() {
+        print("Swift>prepare")
+    }
+
+    static func requiresMainQueueSetup() -> Bool {
+        return true
     }
 }
